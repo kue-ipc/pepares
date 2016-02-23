4be2fd88091462fc1e1a3136c8973f7a9e683477
IGRASHI Makoto <igarashi@kyokyo-u.ac.jp>
2016-02-23 15:46:48 +0900

    HTTP_HOSTを無理矢理修正する

--- webdav.rb
+++ webdav.rb
@@ -14,6 +14,9 @@ class WebDAVFilter
 
   def call(env)
     if env["PATH_INFO"].start_with?(@path)
+      if forwarded_hosts = env["HTTP_X_FORWARDED_HOST"]
+        env["HTTP_HOST"] = forwarded_hosts.split(/,\s?/).last
+      end
       return @dav.call(env)
     else
       return @app.call(env)
