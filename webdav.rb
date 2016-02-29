7fb0504cb499882af1e39d05c3a2b9045d3f424b
IGRASHI Makoto <igarashi@kyokyo-u.ac.jp>
2016-02-29 11:10:38 +0900

    RackDAV::Controller#propfind をハック

--- webdav.rb
+++ webdav.rb
@@ -1,6 +1,52 @@
 # WebDAV Filter
 require "rack_dav"
 
+# hack rack_dav
+# RackDAV::Controller#propfind
+module RackDAV
+  class Controller
+    def propfind
+      raise NotFound if not resource.exist?
+
+      if not request_match("/d:propfind/d:allprop").empty?
+        nodes = all_prop_nodes
+      else
+        nodes = request_match("/d:propfind/d:prop/*")
+        nodes = all_prop_nodes if nodes.empty?
+      end
+
+      nodes.each do |n|
+        # Don't allow empty namespace declarations
+        # See litmus props test 3
+        raise BadRequest if n.namespace.nil? && n.namespace_definitions.empty?
+
+        # Set a blank namespace if one is included in the request
+        # See litmus props test 16
+        # <propfind xmlns="DAV:"><prop><nonamespace xmlns=""/></prop></propfind>
+        if n.namespace.nil?
+          nd = n.namespace_definitions.first
+          if nd.prefix.nil? && nd.href.empty?
+            n.add_namespace(nil, '')
+          end
+        end
+      end
+
+      multistatus do |xml|
+        for resource in find_resources
+          resource.path.gsub!(/\/\//, '/')
+          xml.response do
+            # The path must be finished with  "/" if resource is a collection
+            url = "http://#{host}#{url_escape resource.path}"
+            url += '/' if resource.collection? && ! url.end_with?('/')
+            xml.href url
+            propstats xml, get_properties(resource, nodes)
+          end
+        end
+      end
+    end
+  end
+end
+
 class WebDAVFilter
   def initialize(app, options = {})
     @app = app
@@ -18,14 +64,7 @@ class WebDAVFilter
       if (forwarded_hosts = env["HTTP_X_FORWARDED_HOST"])
         env["HTTP_HOST"] = forwarded_hosts.split(/,\s?/).last
       end
-      res = @dav.call(env)
-      # puts "---request body---"
-      # puts env["rack.input"].gets
-      # puts "---response body---"
-      # res[2].each do |body|
-      #   puts body
-      # end
-      return res
+      @dav.call(env)
     else
       @app.call(env)
     end
@@ -47,12 +86,4 @@ class USBResource < RackDAV::FileResource
       super
     end
   end
-
-    # def property_names
-  #   if stat.directory?
-  #     super - %w(getcontenttype)
-  #   else
-  #     super
-  #   end
-  # end
 end
